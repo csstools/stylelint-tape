9718d3af67571464dab9de1f9a81ab460afded7a
Jonathan Neal <jonathantneal@hotmail.com>
2019-05-13 12:25:40 -0400

    2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changes to stylelint-tape
 
+### 2.0.0 (May 12, 2019)
+
+- Updated: `log-update` to 3.2.0 (major)
+- Updated: peer `stylelint` to 10 (major)
+- Updated: Node 8+ compatibility (major)
+
 ### 1.0.2 (February 27, 2019)
 
 - Fixed an issue where `warnings` could not evaluate strings or key-value pairs.
---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import babel from 'rollup-plugin-babel';
+
+const isCLI = String(process.env.NODE_ENV).includes('cli');
+const input = isCLI ? 'src/cli.js' : 'src/index.js';
+const output = isCLI
+	? { file: 'cli.js', format: 'cjs', sourcemap: true, strict: false }
+: [
+	{ file: 'index.js', format: 'cjs', sourcemap: true, strict: false },
+	{ file: 'index.mjs', format: 'es', sourcemap: true, strict: false }
+];
+
+export default {
+	input,
+	output,
+	plugins: [
+		babel({
+			presets: [
+				['@babel/env', {
+					loose: true,
+					modules: false,
+					targets: { node: 8 },
+					useBuiltIns: 'entry'
+				}]
+			]
+		})
+	].concat(
+		isCLI ? addHashBang()
+		: []
+	)
+};
+
+function addHashBang() {
+	return {
+		name: 'add-hash-bang',
+		renderChunk(code) {
+			return `#!/usr/bin/env node\n${code}`;
+		}
+	};
+}
